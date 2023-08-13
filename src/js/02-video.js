be6c3115d4c2619f40c51606ac3a187d16d65c4a
Schumellman <igor.bondarchuk.1511@gmail.com>
2023-08-13 14:26:18 +0200

    fix 2 task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,11 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-const onPlay = function (data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
-}
 player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+     localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}
 
 const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
 
