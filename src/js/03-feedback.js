559859862eff38e3888e0d5d7e5e8d7282e7e586
Schumellman <igor.bondarchuk.1511@gmail.com>
2023-08-14 22:46:09 +0200

    add and fixed task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
-const throttle = require('lodash.throttle');
+
+import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
 const feedbackEmail = document.querySelector('.feedback-form input');
@@ -18,7 +19,7 @@ function onInputChange() {
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    if (!evt.feedbackEmail || !evt.feedbacktextAria) {
+    if (evt.email.value === undefined || evt.message.value === undefined) {
         alert("please enter all fields correctly");
     }
     else {
@@ -35,3 +36,50 @@ function fillAllInputs() {
         feedbacktextAria.value = saveData.message;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ПРИКЛАД ВИКЛАДАЧА
+
+// import throttle from 'lodash.throttle';
+
+// const feedbackForm = document.querySelector('.feedback-form');
+// const FORM_Key = "feedback-form-state";
+
+
+// feedbackForm.addEventListener('input', throttle(onInputChange, 500));
+// feedbackForm.addEventListener('submit', onFormSubmit);
+
+// let data = JSON.parse(localStorage.getItem(FORM_Key)) ?? {};
+// const { email, message } = feedbackForm.elements;
+
+// email.value = data.email ?? "";
+// message.value = data.message ?? "";
+
+// function onInputChange(evt) {
+//     data[evt.target.name] = evt.target.value;
+//     localStorage.setItem(FORM_Key, JSON.stringify(data));
+// }
+
+// function onFormSubmit(evt) {
+//     evt.preventDefault();
+//     console.log(data);
+//     feedbackForm.reset();
+//     localStorage.removeItem(FORM_Key);
+//     data = {};
+// }
